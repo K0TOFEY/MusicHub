a6cf336333de21f4865b0ac38a17a41865af7c8d
K0T0FEY <kirillperovo2008@gmail.com>
2025-05-16 22:09:56 +0300

    Удаление не нужных файлов

--- app/test_models.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import User, Tag
-
-with app.app_context():
-    user = User(username='testuser', email='test@example.com')
-    tag = Tag(name='testtag')
-    print('kghskjfhsfjsfs')
